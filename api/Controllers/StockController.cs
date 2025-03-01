555503a21160cd653df35ecc51975da5f06d607e
ArtemPoliakov <apoliakov50@gmail.com>
2025-03-01 19:55:09 +0200

    init commit

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Dtos.Stock;
+using api.Intrefaces;
+using api.Mappers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IStockRepository _stockRepo;
+        public StockController(ApplicationDbContext context, IStockRepository stockRepository)
+        {
+            _stockRepo = stockRepository;
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var stocks = await _stockRepo.GetAllAsync();
+
+            var stocksDto = stocks.Select(s => s.toStockDto());
+
+            return Ok(stocksDto);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> getById([FromRoute] int id)
+        {
+            var stock = await _stockRepo.GetByIdAsync(id);
+
+            if (stock == null)
+            {
+                return NotFound();
+            }
+            return Ok(stock.toStockDto());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateStockRequest stockDto)
+        {
+            var stockModel = stockDto.toStockFromCreateDto();
+            await _stockRepo.CreateAsync(stockModel);
+            return CreatedAtAction(nameof(getById), new { id = stockModel.Id }, stockModel.toStockDto());
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockDto stockDto)
+        {
+            var stockModel = await _stockRepo.UpdateAsync(id, stockDto);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stockModel.toStockDto());
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete([FromRoute] int id)
+        {
+            var stockModel = await _stockRepo.DeleteAsync(id);
+
+            if (stockModel == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ api/Repository/CommentRepository.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using api.Intrefaces;
+using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Repository
+{
+    public class CommentRepository : ICommentRepository
+    {
+        private readonly ApplicationDbContext _context;
+        public CommentRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Comment> CreateAsync(Comment commentModel)
+        {
+            await _context.Comment.AddAsync(commentModel);
+            await _context.SaveChangesAsync();
+            return commentModel;
+        }
+
+        public async Task<Comment?> DeleteAsync(int id)
+        {
+            var existingComment = await _context.Comment.FirstOrDefaultAsync(c => c.Id == id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            _context.Remove(existingComment);
+            await _context.SaveChangesAsync();
+
+            return existingComment;
+        }
+
+        public async Task<List<Comment>> GetAllAsync()
+        {
+            return await _context.Comment.ToListAsync();
+        }
+
+        public async Task<Comment?> GetByIdAsync(int id)
+        {
+            return await _context.Comment.FindAsync(id);
+        }
+
+        public async Task<Comment?> UpdateAsync(int id, Comment commentModel)
+        {
+            var existingComment = await _context.Comment.FindAsync(id);
+
+            if (existingComment == null)
+            {
+                return null;
+            }
+
+            existingComment.Title = commentModel.Title;
+            existingComment.Content = commentModel.Content;
+
+            await _context.SaveChangesAsync();
+
+            return existingComment;
+        }
+    }
+}
